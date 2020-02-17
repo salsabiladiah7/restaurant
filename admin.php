6fc414e11eadacabca2867d89ccde7da115de0c2
salsabiladiah7 <salsabila_diah_28rpl@student.smktelkom-mlg.sch.id>
2020-02-17 14:54:49 +0700

    first commit

---
+++ admin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css";>
+    <link rel="stylesheet" type="text/css" href="a.css";>
+    <title>Document</title>
+</head>
+<body>
+<div class=hh>
+    <center><h1  class=a>RM. SELERA<h1></center>
+</div>
+<div class=log>
+    <form action="" method="POST">
+        <br><input type="text" name="ID" placeholder="ID ADMIN"><br><br>
+        <input type="text" name="username" placeholder="USERNAME"><br><br>
+        <input type="text" name="password" placeholder="PASSWORD"><br><br>
+        <button type="submit" class=lin>LOGIN</button><br><br>
+        <a class=link href="login.php">KEMBALI</a>
+    </form>
+</div>
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host ="localhost";
+$username="root";
+$password="";
+$database="admin";
+$connect = mysqli_connect($host, $username,$password. $database);
+?>
